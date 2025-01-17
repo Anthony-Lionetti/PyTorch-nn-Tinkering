8112e1f653784a06e6cc871e56fb04cf87f04c35
Anthony Lionetti <lionetti.tech@gmail.com>
2025-01-16 20:36:58 -0500

    add RMSNorm layer class

--- llama_play/Llama1b.py
+++ llama_play/Llama1b.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Llama1b:
+    block_size = 2048
     vocab_size = 128256
     layers = 16
     attn_heads = 16
@@ -41,9 +42,31 @@ class LlamaMLP(nn.Module):
 
 
 class LlamaRMSNorm(nn.Module):
+    """
+    Root Mean Square Norm
+    ---
+    Differs from the traditional transformer Architecture's LayerNorm.
+    RMS is computationally more efficient, but slightly less accurate. 
+    In practice, the perfromance differences are negligible.
+    """
 
-    def __init__(self, dim:tuple, lr:float):
+    def __init__(self, hidden_size:tuple, eps:1e-6):
         super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states:torch.Tensor):
+        # convert hidden states to float32
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        # Square the terms, and calculate the mean
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        # return the normalized weights
+        return self.weight * hidden_states.to(input_dtype)
+
+    def extra_repr(self):
+        return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
 
 
 class LlamaRotaryEmbeddings(nn.Module):
