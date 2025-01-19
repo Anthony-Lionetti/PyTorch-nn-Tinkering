2b242ee7551c2a7ee0dfaaa63b52ec4dcbb440cb
Anthony Lionetti <lionetti.tech@gmail.com>
2025-01-18 20:00:58 -0500

    play with vector tranformations to refresh on matrix transformations

--- llama_play/Llama1b.py
+++ llama_play/Llama1b.py
@@ -1,21 +1,16 @@
 import torch
 import torch.nn as nn
-from dataclasses import dataclass
+from typing import Tuple, Optional
+from configs import LlamaConfig, llama_3_2_1b
 
-@dataclass
-class LlamaConfig:
-    block_size = 2_048
-    vocab_size = 128_256
-    n_embd = 2_048
-    intermediate_size = 8_192
-    layers = 16
-    attn_heads = 32
-    rope_theta = 500_000
-    attention_dropout = 0.0
-    attention_bias = False
-    mlp_bias = False
+# View the configs.py file to understand this configuration abstraction
+# essentially, this is where all of the parameters for initializing
+# the model size and rope embeddings
+config = LlamaConfig(**llama_3_2_1b)
 
-
+################################
+## Llama Model Implementation ##
+################################
 class LlamaModel(nn.Module):
 
     def __init__(self, config:LlamaConfig):
@@ -24,54 +19,31 @@ class LlamaModel(nn.Module):
 
         self.model = nn.ModuleDict(dict(
             embed_tokens = nn.Embedding(config.vocab_size, config.n_embd),
-            self_attn = nn.ModuleList([LlamaAttention(config) for _ in range(config.layers)]),
+            self_attn = nn.ModuleList([LlamaDecoderBlock(config) for _ in range(config.layers)]),
             norm = LlamaRMSNorm((2048,), eps=1e-5),
             rotary_emb = LlamaRotaryEmbedding(),
         ))
 
         self.lm_head = nn.Embedding(config.n_embd, config.vocab_size, bias=False)
+    
+    def forward(self, x):
+        ...
 
-class LlamaAttention(nn.Module):
-    """Multi-headed attention from 'Attention Is All You Need' paper"""
+#########################
+## Llama Decoder Block ##
+#########################
+class LlamaDecoderBlock(nn.Module):
+    ...
 
-    def __init__(self, config: LlamaConfig, layer_idx: int):
-        super().__init__()
-        self.config = config
-        self.layer_idx = layer_idx
-        # Gets the head dimension from the config, or returns a default head_dim calculation
-        self.head_dim = getattr(config, "head_dim", config.n_embd // config.attn_heads)
-        self.num_key_value_groups = config.attn_heads // config.num_key_value_heads
-        self.scaling = self.head_dim**-0.5
-        self.attention_dropout = config.attention_dropout
-        self.is_causal = True
-
-        self.q_proj = nn.Linear(config.n_embd, config.attn_heads * self.head_dim, bias=config.attention_bias)
-        self.k_proj = nn.Linear(config.n_embd, config.num_key_value_heads * self.head_dim, bias=config.attention_bias)
-        self.v_proj = nn.Linear(config.n_embd, config.num_key_value_heads * self.head_dim, bias=config.attention_bias)
-        self.o_proj = nn.Linear(config.attn_heads * self.head_dim, config.n_embd, bias=config.attention_bias)
-
-    def forward(self,x: torch.Tensor, position_embeddings:tuple[torch.Tensor, torch.Tensor]):
-        input_shape = x.shape[:-1]
-        hidden_shape = (*input_shape, -1, self.head_dim)
-
-        query_states = self.q_proj(x).view(hidden_shape).transpose(1, 2)
-        key_states = self.k_proj(x).view(hidden_shape).transpose(1, 2)
-        value_states = self.v_proj(x).view(hidden_shape).transpose(1, 2)
-
-        cos, sin = position_embeddings
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
-
-        attn_output = attn_output.reshape(*input_shape, -1).contiguous()
-        attn_output = self.o_proj(attn_output)
-        return attn_output, attn_weights
-
+####################################
+## Llama Feed Forward Layer (MLP) ##
+####################################
 class LlamaMLP(nn.Module):
-    # TODO: Sort out sizes and fix LLamaConfig accordingly
 
     def __init__(self, config:LlamaConfig):
         super().__init__()
         self.config = config
-        self.hidden_size = config.n_embd
+        self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
         self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
         self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
@@ -82,35 +54,31 @@ class LlamaMLP(nn.Module):
         down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
         return down_proj
 
+#############
+## RMSNorm ##
+#############
 class LlamaRMSNorm(nn.Module):
     """
     Root Mean Square Norm
-    ---
-    Differs from the traditional transformer Architecture's LayerNorm.
-    RMS is computationally more efficient, but slightly less accurate. 
-    In practice, the perfromance differences are negligible.
     """
-    def __init__(self, hidden_size:tuple, eps:1e-6):
+    def __init__(self, hidden_size:torch.tensor, eps:float=1e-6):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = eps
 
-    def forward(self, hidden_states:torch.Tensor):
-        # convert hidden states to float32
+    def forward(self, hidden_states:torch.Tensor) -> torch.Tensor:
         input_dtype = hidden_states.dtype
         hidden_states = hidden_states.to(torch.float32)
-        # Square the terms, and calculate the mean
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        # return the normalized weights
         return self.weight * hidden_states.to(input_dtype)
 
     def extra_repr(self):
         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
 
-############################
-# Helper Functions for RoPE #
-############################
+######################
+## Rotary Embedding ##
+######################
 
 def rotate_half(x):
     """Rotates half the hidden dims of the input."""
@@ -126,9 +94,21 @@ def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
 
+
 class LlamaRotaryEmbedding(nn.Module):
-    def __init__(self, dim: int, max_position_embeddings: int = 2048, base: int = 10000):
+    def __init__(self, config:LlamaConfig):
         super().__init__()
+
+        # BC: "rope_type" was originally "type"
+        self.max_seq_len_cached = config.max_position_embeddings
+        self.original_max_seq_len = config.max_position_embeddings
+
+        self.config = config
+        self.rope_init_fn = ROPE_INIT_FUNCTIONS[self.rope_type]
+
+        inv_freq, self.attention_scaling = self.rope_init_fn(self.config, device)
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.original_inv_freq = self.inv_freq
         
         # Create inverse frequency bands
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
--- llama_play/configs.py
+++ llama_play/configs.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
+from typing import List
+
 llama_3_2_1b = {
-  "architectures": [
-    "LlamaForCausalLM"
-  ],
   "attention_bias": False,
   "attention_dropout": 0.0,
   "bos_token_id": 128000,
@@ -33,3 +33,39 @@ llama_3_2_1b = {
   "use_cache": True,
   "vocab_size": 128256
 }
+
+
+@dataclass
+class RopeScaling:
+    factor: float
+    high_freq_factor: float
+    low_freq_factor: float
+    original_max_position_embeddings: int
+    rope_type: str
+
+@dataclass
+class LlamaConfig:
+    attention_bias: bool
+    attention_dropout: float
+    bos_token_id: int
+    eos_token_id: int
+    head_dim: int
+    hidden_act: str
+    hidden_size: int
+    initializer_range: float
+    intermediate_size: int
+    max_position_embeddings: int
+    mlp_bias: bool
+    model_type: str
+    num_attention_heads: int
+    num_hidden_layers: int
+    num_key_value_heads: int
+    pretraining_tp: int
+    rms_norm_eps: float
+    rope_scaling: RopeScaling
+    rope_theta: float
+    tie_word_embeddings: bool
+    torch_dtype: str
+    transformers_version: str
+    use_cache: bool
+    vocab_size: int
